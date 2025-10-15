9df198304dc2e8418d059d84d02e9f62376ec490
NinoFischer <nino.fischer.pala@gmail.com>
2025-10-15 21:55:19 +0800

    Add polls app to settings and define models for Customer, Topic, and Question

--- Astonishing/polls/models.py
+++ Astonishing/polls/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length= 250)
+
+    def __str__(self):
+        return self.name
+    
