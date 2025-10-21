93d9c1eeaf1092e3398908db08c5a2e2d41d2289
NinoFischer <nino.fischer.pala@gmail.com>
2025-10-21 21:00:07 +0800

    Refactor polls app: add app_name for namespacing, improve topic view error handling, and update URL patterns. Modify index and topic templates to use Django's URL template tag for better routing.

--- Astonishing/polls/views.py
+++ Astonishing/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.template import context, loader
 
 
@@ -11,14 +11,17 @@ from .models import Topic
 
 def index(request):
     latest_topic_list = Topic.objects.all().order_by("-created_at")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_topic_list":latest_topic_list}
     
-
-    return HttpResponse(render(request,"polls/index.html", context))
+    return render(request, "polls/index.html", context)
 
 def topic(request, topic_name):
+    try:
+        topic = Topic.objects.get(name=topic_name)
+    except:
+        raise Http404("Topic does not exist.")
+        
     template = loader.get_template("polls/topic.html")
-    context = {"topic_name": topic_name}
+    context = {"topic_name": topic_name, "topic": topic}
 
     return HttpResponse(template.render(context,template)) #HttpResponse(render(request, f"polls/topic.html"), context)
