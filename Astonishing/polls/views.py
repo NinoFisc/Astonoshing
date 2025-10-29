5e73bfb9e0ccee820ca15f1c07dc96b8330c02c1
NinoFischer <nino.fischer.pala@gmail.com>
2025-10-29 12:54:22 +0800

    Enhance topic view to handle question submissions and update template for dynamic form rendering. Modify imports in views.py for new functionality and ensure proper context handling.

--- Astonishing/polls/views.py
+++ Astonishing/polls/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from django.http import Http404, HttpResponse
 from django.template import context, loader
 
 
+
 from .models import Topic,Question,Customer
+
+from .forms import QuestionForm
 
 # Create your views here.
 
@@ -13,17 +16,50 @@ def index(request):
     latest_topic_list = Topic.objects.all().order_by("-created_at")[:5]
     context = {"latest_topic_list":latest_topic_list}
     
-    return render(request, "polls/index.html", context)
+    return render(request,
+    "polls/index.html",
+    context)
 
 def topic(request, topic_name):
     try:
+
         topic = Topic.objects.get(name=topic_name)
         questions = Question.objects.filter(topic=topic)
+
+        if request.method == 'POST':
+            form = QuestionForm(request.POST)
+
+            if form.is_valid():
+
+                question = form.save(commit=False)
+                question.topic = topic
+                question.save()
+        
+
+
+        if request.method == 'GET':
+            form = QuestionForm()
 
     except:
         raise Http404("Topic does not exist.")
         
-    template = loader.get_template("polls/topic.html")
-    context = {"topic_name": topic_name, "topic": topic,'questions':questions}
+    context = {
+        "topic_name": topic_name,
+        "topic": topic,
+        'questions':questions,
+        "form":form
+        }
 
-    return HttpResponse(template.render(context,template)) #HttpResponse(render(request, f"polls/topic.html"), context)
+    return render(
+    request,
+    "polls/topic.html",
+    context
+      ) #HttpResponse(render(request, f"polls/topic.html"), context)
+
+
+
+
+
+
+
+        
